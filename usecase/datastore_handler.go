b6e303dd82b9b9669fd869d52160182817b2d4d8
yuichi10 <y_084743@yahoo.co.jp>
2019-01-22 01:30:12 +0900

    add set-url usease

--- usecase/datastore_handler.go
+++ usecase/datastore_handler.go
@@ -5,3 +5,7 @@ import "github.com/yuichi10/jiractl/entity"
 type ICredentialDataAccess interface {
 	AddCredential(credentialName, userName, basicAuth string) entity.Credential
 }
+
+type IJiraURLDataAccess interface {
+	AddJiraURL(name, url string) *entity.JiraURL
+}
