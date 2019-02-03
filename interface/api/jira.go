efd1efad376ab09b6623a30511a934700beaeab1
ysawada <ysawada@yahoo-corp.jp>
2019-02-04 01:08:21 +0900

    add interface and struct for api

--- interface/api/jira.go
+++ interface/api/jira.go
@@ -1,4 +1,11 @@
 package api
 
 func GetCurrentSprintIssues() {
+	// TODO: Get Board info
+	// /rest/agile/1.0/board?name=DPS-CD1 7661
+	// TODO: Get all sprint info
+	// /rest/agile/1.0/board/7661/sprint
+	// TODO: get active sprint id (state: active) 16245
+	// TODO: get sprint issues
+	// /rest/agile/1.0/sprint/16245/issue
 }
